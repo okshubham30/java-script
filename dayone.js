798625a78b4cbde0ab4ee1e8c15cd949a8c52342
okshubham32 <shubham16maury@gmail.com>
2026-06-02 12:53:47 +0530

    pdf one for day one

---
+++ dayone.js
@@ -0,0 +1,26 @@
+// question one of pdf one 
+
+const product = {
+    name: "Parker Jotter Standard CT Ball Point Pen",
+    rating: 5,
+    reviews: 7002, 
+    price: 270, 
+    offer: "5% off",
+
+};
+
+console.log(product);
+
+// question two of pdf one 
+
+const profile = {
+    username: "okshubham32",
+    posts: 5,
+    followers: "7k",
+    following: 12,
+    name: "Shubham Maurya",
+    profession: "footballer",
+
+};
+
+console.log(profile);
