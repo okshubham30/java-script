5fe785f07acf03f447442c3ffec13bf2c627466f
okshubham32 <shubham16maury@gmail.com>
2026-06-16 13:18:51 +0530

    tic tac toe game  practice
    tic tac toe game  practice

---
+++ Tic-Tac-Toe/app.js
@@ -0,0 +1,24 @@
+let usreScore = 0;
+let compScore = 0;
+
+const choices = document.querySelectorAll(".choices");
+
+const genCompChoice = () => {
+    let options = ["rock", "paper", "scissors"];
+    const randIdx= Math.floor(Math.randon()*3);
+    return options[randIdx];
+}
+
+const playGame= (userChoice) => {
+    console.log("user choice =", userChoice);
+    //generate computer choice -> modular
+    const compChoice= genCompChoice();
+    console.log("comp choice =", compChoice);
+}
+choices.forEach((choice) => {
+    choice.addEventListener("click", () => {
+        const userChoice = choice.getAttribute("id");
+        playGame(userChoice);
+    })
+})
+
